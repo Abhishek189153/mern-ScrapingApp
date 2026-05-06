8d18cc5ed5a197e3b9c946d00065dfd79da4d268
Abhishek Pant <abhi2002pant@gmail.com>
2026-05-07 01:10:14 +0530

    Add authentication APIs

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const scrapeRoutes = require("./routes/scrapeRoutes");
+const authRoutes = require("./routes/authRoutes");
 const scrapeHackerNews = require("./scraper/hackerNewsScraper");
 
 dotenv.config();
@@ -22,6 +23,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+app.use("/api/auth", authRoutes);
 app.use("/api/scrape", scrapeRoutes);
 
 const PORT = process.env.PORT || 5000;
